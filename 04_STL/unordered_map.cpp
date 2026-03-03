054cf4df71ce7582997d4ab820eba604bc4b49fa
8BitsLife <gajanan.prsnl@gmail.com>
2026-03-03 18:10:56 +0530

    Update unordered_map.cpp

--- 04_STL/unordered_map.cpp
+++ 04_STL/unordered_map.cpp
@@ -1,3 +1,21 @@
-
+#include <iostream>
 #include <unordered_map>
 using namespace std;
+
+int main()
+{
+    unordered_map<string, int> marks;
+
+    marks["Math"] = 90;
+    marks["Physics"] = 85;
+    marks["Chemistry"] = 88;
+
+    cout << "Subject Marks:\n";
+    for (auto pair : marks)
+    {
+        cout << pair.first 
+             << " : " << pair.second << endl;
+    }
+
+    return 0;
+}
