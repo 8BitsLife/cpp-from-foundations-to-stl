126a34a43d826c1705fc795a3c35e0447c0cf1db
8BitsLife <gajanan.prsnl@gmail.com>
2026-03-03 18:35:24 +0530

    Add Counter class with prefix increment operator
    Implemented a Counter class with prefix increment operator overloading.

--- 02_OOP/03_Operator_Overloading/prefix_increment.cpp
+++ 02_OOP/03_Operator_Overloading/prefix_increment.cpp
@@ -1,7 +1,42 @@
+#include <iostream>
+using namespace std;
 
-class Num{
-    int val;
+// Counter Class
+class Counter
+{
+private:
+    int value;
+
 public:
-    Num(int v):val(v){}
-    Num& operator++(){ ++val; return *this; }
+    Counter(int v = 0)
+    {
+        value = v;
+    }
+
+    void display() const
+    {
+        cout << "Value: " << value << endl;
+    }
+
+    // Prefix Increment Overloading
+    Counter operator++()
+    {
+        ++value;  // increment first
+        return *this; // return updated object
+    }
 };
+
+int main()
+{
+    Counter c1(10);
+
+    cout << "Before Prefix Increment:\n";
+    c1.display();
+
+    ++c1;  // Calls overloaded operator
+
+    cout << "After Prefix Increment:\n";
+    c1.display();
+
+    return 0;
+}
