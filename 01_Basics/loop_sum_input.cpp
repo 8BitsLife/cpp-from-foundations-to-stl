d8cd185dd911810cc5892bb5d7afbb017475ed4f
8BitsLife <gajanan.prsnl@gmail.com>
2026-03-03 21:39:09 +0530

    Update loop_sum_input.cpp

--- 01_Basics/loop_sum_input.cpp
+++ 01_Basics/loop_sum_input.cpp
@@ -1,10 +1,27 @@
-
 #include <iostream>
 using namespace std;
 
-int main() {
-    int value, sum = 0;
-    while (cin >> value)
-        sum += value;
-    cout << "Sum = " << sum << endl;
+// Program: Loop Sum Input
+// Purpose: Take multiple inputs using loop and calculate sum
+
+int main()
+{
+    int n;
+    cout << "How many numbers do you want to enter? ";
+    cin >> n;
+
+    int number;
+    int sum = 0;
+
+    for (int i = 1; i <= n; i++)
+    {
+        cout << "Enter number " << i << ": ";
+        cin >> number;
+
+        sum += number;  // adding to sum
+    }
+
+    cout << "\nTotal Sum = " << sum << endl;
+
+    return 0;
 }
