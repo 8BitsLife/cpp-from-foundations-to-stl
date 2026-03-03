799fbb21cea92d400b2d34fccde4f551f30462bd
8BitsLife <gajanan.prsnl@gmail.com>
2026-03-03 21:23:51 +0530

    Add Shape class with Square and Circle derived classes
    Implemented a hierarchical inheritance structure with Shape as the base class and Square and Circle as derived classes. Added area calculation methods for both derived classes.

--- 02_OOP/02_Inheritance/hierarchical.cpp
+++ 02_OOP/02_Inheritance/hierarchical.cpp
@@ -1,4 +1,48 @@
+#include <iostream>
+using namespace std;
 
-class A{};
-class B : public A{};
-class C : public A{};
+// Base Class
+class Shape
+{
+protected:
+    float value;
+
+public:
+    void setValue(float v)
+    {
+        value = v;
+    }
+};
+
+// Derived Class 1
+class Square : public Shape
+{
+public:
+    float area()
+    {
+        return value * value;
+    }
+};
+
+// Derived Class 2
+class Circle : public Shape
+{
+public:
+    float area()
+    {
+        return 3.14 * value * value;
+    }
+};
+
+int main()
+{
+    Square s;
+    s.setValue(5);
+    cout << "Square Area: " << s.area() << endl;
+
+    Circle c;
+    c.setValue(4);
+    cout << "Circle Area: " << c.area() << endl;
+
+    return 0;
+}
