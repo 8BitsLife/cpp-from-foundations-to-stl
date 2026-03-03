12ca7e05d8a6c019f9764ec79d13f41cc6e4906e
8BitsLife <gajanan.prsnl@gmail.com>
2026-03-03 18:09:25 +0530

    Update set.cpp

--- 04_STL/set.cpp
+++ 04_STL/set.cpp
@@ -1,3 +1,21 @@
-
+#include <iostream>
 #include <set>
 using namespace std;
+
+int main()
+{
+    set<int> s;
+
+    s.insert(10);
+    s.insert(20);
+    s.insert(10); // duplicate ignored
+
+    cout << "Set elements (sorted, unique): ";
+    for (int x : s)
+        cout << x << " ";
+
+    if (s.find(20) != s.end())
+        cout << "\n20 found in set";
+
+    return 0;
+}
