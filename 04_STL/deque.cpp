ed885254739d23af9f769bad9ff4de1a9514c3b6
8BitsLife <gajanan.prsnl@gmail.com>
2026-03-03 18:09:54 +0530

    Implement basic deque operations in deque.cpp

--- 04_STL/deque.cpp
+++ 04_STL/deque.cpp
@@ -1,3 +1,25 @@
-
+#include <iostream>
 #include <deque>
 using namespace std;
+
+int main()
+{
+    deque<int> dq;
+
+    dq.push_back(10);
+    dq.push_front(5);
+    dq.push_back(20);
+
+    cout << "Deque elements: ";
+    for (int x : dq)
+        cout << x << " ";
+
+    dq.pop_front();
+    dq.pop_back();
+
+    cout << "\nAfter popping from both ends: ";
+    for (int x : dq)
+        cout << x << " ";
+
+    return 0;
+}
