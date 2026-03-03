095f3840e145b22dc35c53bf799b48b1a21a4b88
8BitsLife <gajanan.prsnl@gmail.com>
2026-03-03 18:13:05 +0530

    Implement student list display using map
    Added a simple program to display a list of students with their roll numbers.

--- 04_STL/map.cpp
+++ 04_STL/map.cpp
@@ -1,3 +1,21 @@
-
+#include <iostream>
 #include <map>
 using namespace std;
+
+int main()
+{
+    map<int, string> students;
+
+    students[1] = "Alice";
+    students[2] = "Bob";
+    students[3] = "Charlie";
+
+    cout << "Student List:\n";
+    for (auto pair : students)
+    {
+        cout << "Roll No: " << pair.first 
+             << " Name: " << pair.second << endl;
+    }
+
+    return 0;
+}
