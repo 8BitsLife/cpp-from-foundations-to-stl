c03708a1afba131f486bcbc8d553c897c2893640
8BitsLife <gajanan.prsnl@gmail.com>
2026-03-03 21:30:35 +0530

    Implement Student class with copy constructor
    Added a Student class with a copy constructor and display method.

--- 02_OOP/01_Constructors/copy_constructor.cpp
+++ 02_OOP/01_Constructors/copy_constructor.cpp
@@ -1,10 +1,40 @@
-
 #include <iostream>
 using namespace std;
 
-class Box {
-    int value;
+// Copy Constructor Example
+
+class Student
+{
+private:
+    int roll;
+
 public:
-    Box(int v):value(v){}
-    Box(const Box &b){ value = b.value; }
+    Student(int r)
+    {
+        roll = r;
+        cout << "Constructor Called\n";
+    }
+
+    // Copy Constructor
+    Student(const Student &obj)
+    {
+        roll = obj.roll;
+        cout << "Copy Constructor Called\n";
+    }
+
+    void display() const
+    {
+        cout << "Roll: " << roll << endl;
+    }
 };
+
+int main()
+{
+    Student s1(101);
+
+    Student s2 = s1;  // copy constructor called
+
+    s2.display();
+
+    return 0;
+}
