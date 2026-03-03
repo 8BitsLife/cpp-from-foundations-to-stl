99d205c2429091e7b1a4034fcf7ee894bc4695ef
8BitsLife <gajanan.prsnl@gmail.com>
2026-03-03 18:34:18 +0530

    Implement Complex class with operator overloading
    Added a Complex class with overloaded + operator for adding complex numbers.

--- 02_OOP/03_Operator_Overloading/arithmetic_plus.cpp
+++ 02_OOP/03_Operator_Overloading/arithmetic_plus.cpp
@@ -1,7 +1,56 @@
+#include <iostream>
+using namespace std;
 
-class Num{
-    int val;
+// Class to represent Complex Numbers
+class Complex
+{
+private:
+    int real;
+    int imag;
+
 public:
-    Num(int v):val(v){}
-    Num operator+(Num n){ return Num(val+n.val); }
+    // Constructor
+    Complex(int r = 0, int i = 0)
+    {
+        real = r;
+        imag = i;
+    }
+
+    // Function to display complex number
+    void display() const
+    {
+        cout << real << " + " << imag << "i" << endl;
+    }
+
+    // Overloading + operator
+    Complex operator+(const Complex &obj) const
+    {
+        Complex temp;
+
+        temp.real = real + obj.real;
+        temp.imag = imag + obj.imag;
+
+        return temp;
+    }
 };
+
+// MAIN
+int main()
+{
+    Complex c1(3, 4);
+    Complex c2(5, 6);
+
+    cout << "First Complex Number: ";
+    c1.display();
+
+    cout << "Second Complex Number: ";
+    c2.display();
+
+    // Using overloaded + operator
+    Complex c3 = c1 + c2;
+
+    cout << "After Addition: ";
+    c3.display();
+
+    return 0;
+}
