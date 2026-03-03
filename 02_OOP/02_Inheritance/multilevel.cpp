15c99bd0706c7e285e054b3ca8c55f71f643c84f
8BitsLife <gajanan.prsnl@gmail.com>
2026-03-03 21:23:22 +0530

    Add multilevel inheritance example with display method
    Implemented a multilevel inheritance structure with Grandparent, Parent, and Child classes. Added methods to set and display family, parent, and child names.

--- 02_OOP/02_Inheritance/multilevel.cpp
+++ 02_OOP/02_Inheritance/multilevel.cpp
@@ -1,4 +1,59 @@
+#include <iostream>
+using namespace std;
 
-class A{};
-class B : public A{};
-class C : public B{};
+// Level 1
+class Grandparent
+{
+protected:
+    string familyName;
+
+public:
+    void setFamily(string f)
+    {
+        familyName = f;
+    }
+};
+
+// Level 2
+class Parent : public Grandparent
+{
+protected:
+    string parentName;
+
+public:
+    void setParent(string p)
+    {
+        parentName = p;
+    }
+};
+
+// Level 3
+class Child : public Parent
+{
+private:
+    string childName;
+
+public:
+    void setChild(string f, string p, string c)
+    {
+        setFamily(f);
+        setParent(p);
+        childName = c;
+    }
+
+    void display() const
+    {
+        cout << "Family Name: " << familyName << endl;
+        cout << "Parent Name: " << parentName << endl;
+        cout << "Child Name : " << childName << endl;
+    }
+};
+
+int main()
+{
+    Child c;
+    c.setChild("Sharma", "Rajesh", "Aman");
+    c.display();
+
+    return 0;
+}
