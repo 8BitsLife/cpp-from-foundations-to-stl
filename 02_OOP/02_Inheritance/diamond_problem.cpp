d9ba93f9eddccc4c253fe3b097912b60576bf7bf
8BitsLife <gajanan.prsnl@gmail.com>
2026-03-03 21:25:03 +0530

    Add virtual inheritance example with Person, Teacher, and Researcher
    Implemented virtual inheritance to avoid duplication in derived classes.

--- 02_OOP/02_Inheritance/diamond_problem.cpp
+++ 02_OOP/02_Inheritance/diamond_problem.cpp
@@ -1,5 +1,56 @@
+#include <iostream>
+using namespace std;
 
-class A{};
-class B : virtual public A{};
-class C : virtual public A{};
-class D : public B, public C{};
+// Base Class
+class Person
+{
+protected:
+    string name;
+
+public:
+    void setName(string n)
+    {
+        name = n;
+    }
+};
+
+// Using virtual inheritance to avoid duplication
+class Teacher : virtual public Person
+{
+public:
+    void teach()
+    {
+        cout << name << " is teaching." << endl;
+    }
+};
+
+class Researcher : virtual public Person
+{
+public:
+    void research()
+    {
+        cout << name << " is researching." << endl;
+    }
+};
+
+// Derived from both
+class Professor : public Teacher, public Researcher
+{
+public:
+    void show()
+    {
+        cout << "Professor Name: " << name << endl;
+    }
+};
+
+int main()
+{
+    Professor p;
+    p.setName("Dr. Sharma");
+
+    p.show();
+    p.teach();
+    p.research();
+
+    return 0;
+}
