4f33829423e520a596977ad71aeee55b637af89b
8BitsLife <gajanan.prsnl@gmail.com>
2026-03-03 18:27:06 +0530

    Implement multiple catch blocks for user-defined exceptions
    Added user choice for exception type and handled multiple exceptions.

--- 02_OOP/04_Exception_Handling/multiple_catch.cpp
+++ 02_OOP/04_Exception_Handling/multiple_catch.cpp
@@ -1,4 +1,51 @@
+#include <iostream>
+#include <string>
+using namespace std;
 
-try{ throw 3.14; }
-catch(int){}
-catch(double){}
+int main()
+{
+    int choice;
+    cout << "Choose exception type:\n";
+    cout << "1. Integer\n2. Double\n3. String\n";
+    cin >> choice;
+
+    try
+    {
+        if (choice == 1)
+        {
+            throw 10;
+        }
+        else if (choice == 2)
+        {
+            throw 3.14;
+        }
+        else if (choice == 3)
+        {
+            throw string("Custom string error!");
+        }
+        else
+        {
+            throw "Unknown error occurred!";
+        }
+    }
+    catch (int e)
+    {
+        cout << "Integer exception caught: " << e << endl;
+    }
+    catch (double e)
+    {
+        cout << "Double exception caught: " << e << endl;
+    }
+    catch (string e)
+    {
+        cout << "String exception caught: " << e << endl;
+    }
+    catch (...)
+    {
+        cout << "Default catch block: Unknown exception caught!" << endl;
+    }
+
+    cout << "\nProgram safely exited." << endl;
+
+    return 0;
+}
