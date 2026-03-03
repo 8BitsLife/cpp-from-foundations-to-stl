778ffa19184c49f8a9ad3fea8a4468858ea11dcc
8BitsLife <gajanan.prsnl@gmail.com>
2026-03-03 18:11:41 +0530

    Implement basic priority queue example
    Added a simple example of using a priority queue in C++.

--- 04_STL/priority_queue.cpp
+++ 04_STL/priority_queue.cpp
@@ -1,3 +1,23 @@
-
+#include <iostream>
 #include <queue>
 using namespace std;
+
+int main()
+{
+    // Max heap by default
+    priority_queue<int> pq;
+
+    pq.push(10);
+    pq.push(50);
+    pq.push(20);
+
+    cout << "Elements in priority order:\n";
+
+    while (!pq.empty())
+    {
+        cout << pq.top() << " ";
+        pq.pop();
+    }
+
+    return 0;
+}
