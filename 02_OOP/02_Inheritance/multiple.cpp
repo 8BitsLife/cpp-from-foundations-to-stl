413e6325badead7764bfa3383a240c8111e4eded
8BitsLife <gajanan.prsnl@gmail.com>
2026-03-03 21:24:30 +0530

    Implement multiple inheritance with Academic and Sports

--- 02_OOP/02_Inheritance/multiple.cpp
+++ 02_OOP/02_Inheritance/multiple.cpp
@@ -1,4 +1,50 @@
+#include <iostream>
+using namespace std;
 
-class A{};
-class B{};
-class C : public A, public B{};
+// Base Class 1
+class Academic
+{
+protected:
+    int marks;
+
+public:
+    void setMarks(int m)
+    {
+        marks = m;
+    }
+};
+
+// Base Class 2
+class Sports
+{
+protected:
+    int score;
+
+public:
+    void setScore(int s)
+    {
+        score = s;
+    }
+};
+
+// Derived Class
+class Result : public Academic, public Sports
+{
+public:
+    void display()
+    {
+        cout << "Academic Marks: " << marks << endl;
+        cout << "Sports Score  : " << score << endl;
+        cout << "Total         : " << marks + score << endl;
+    }
+};
+
+int main()
+{
+    Result r;
+    r.setMarks(80);
+    r.setScore(20);
+    r.display();
+
+    return 0;
+}
