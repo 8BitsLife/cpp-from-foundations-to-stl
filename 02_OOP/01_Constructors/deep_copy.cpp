42bb43cc39f6e884dba5232689c074b357f77416
8BitsLife <gajanan.prsnl@gmail.com>
2026-03-03 21:32:20 +0530

    Implement deep copy constructor in Demo class

--- 02_OOP/01_Constructors/deep_copy.cpp
+++ 02_OOP/01_Constructors/deep_copy.cpp
@@ -1,11 +1,54 @@
-
 #include <iostream>
 using namespace std;
 
-class Deep {
-    int* data;
+// Deep Copy Example
+
+class Demo
+{
+private:
+    int *ptr;
+
 public:
-    Deep(int v){ data = new int(v); }
-    Deep(const Deep &d){ data = new int(*d.data); }
-    ~Deep(){ delete data; }
+    Demo(int value)
+    {
+        ptr = new int(value);
+    }
+
+    // Deep Copy Constructor
+    Demo(const Demo &obj)
+    {
+        ptr = new int(*(obj.ptr));  // allocate new memory
+        cout << "Deep Copy Constructor Called\n";
+    }
+
+    void setValue(int value)
+    {
+        *ptr = value;
+    }
+
+    void display() const
+    {
+        cout << "Value: " << *ptr << endl;
+    }
+
+    ~Demo()
+    {
+        delete ptr;
+    }
 };
+
+int main()
+{
+    Demo d1(10);
+    Demo d2 = d1;   // deep copy
+
+    d2.setValue(50);
+
+    cout << "d1: ";
+    d1.display();
+
+    cout << "d2: ";
+    d2.display();
+
+    return 0;
+}
