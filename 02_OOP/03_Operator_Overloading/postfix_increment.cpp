58f5ce2fda3f98141d5cbfb302d5fb407f9a3282
8BitsLife <gajanan.prsnl@gmail.com>
2026-03-03 18:36:03 +0530

    Add Counter class with postfix increment operator
    Implemented a Counter class with postfix increment operator overloading.

--- 02_OOP/03_Operator_Overloading/postfix_increment.cpp
+++ 02_OOP/03_Operator_Overloading/postfix_increment.cpp
@@ -1,7 +1,45 @@
+#include <iostream>
+using namespace std;
 
-class Num{
-    int val;
+class Counter
+{
+private:
+    int value;
+
 public:
-    Num(int v):val(v){}
-    Num operator++(int){ Num temp=*this; val++; return temp; }
+    Counter(int v = 0)
+    {
+        value = v;
+    }
+
+    void display() const
+    {
+        cout << "Value: " << value << endl;
+    }
+
+    // Postfix increment overloading
+    Counter operator++(int)  // int is dummy parameter
+    {
+        Counter temp = *this; // store old value
+        value++;              // increment after
+        return temp;          // return old value
+    }
 };
+
+int main()
+{
+    Counter c1(20);
+
+    cout << "Before Postfix Increment:\n";
+    c1.display();
+
+    Counter c2 = c1++;  // old value goes to c2
+
+    cout << "\nAfter Postfix Increment:\n";
+    c1.display();
+
+    cout << "Returned Old Value:\n";
+    c2.display();
+
+    return 0;
+}
