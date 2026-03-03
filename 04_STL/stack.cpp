0a842f7d5fd5d8d4cb50a204a42e2ebb03db4b3e
8BitsLife <gajanan.prsnl@gmail.com>
2026-03-03 18:06:24 +0530

    Implement basic stack operations in stack.cpp
    Added a simple stack implementation using C++ STL.

--- 04_STL/stack.cpp
+++ 04_STL/stack.cpp
@@ -1,3 +1,24 @@
-
+#include <iostream>
 #include <stack>
 using namespace std;
+
+int main()
+{
+    stack<int> st;
+
+    // Pushing elements
+    st.push(10);
+    st.push(20);
+    st.push(30);
+
+    cout << "Top element: " << st.top() << endl;
+
+    // Removing elements
+    while (!st.empty())
+    {
+        cout << "Popping: " << st.top() << endl;
+        st.pop();
+    }
+
+    return 0;
+}
