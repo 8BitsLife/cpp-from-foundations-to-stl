d4ae5bddeedf85e95925db8526d51ce94c808816
8BitsLife <gajanan.prsnl@gmail.com>
2026-03-03 21:44:23 +0530

    Create pointer_basics.cpp

---
+++ 01_Basics/pointer_basics.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x = 10;
+
+    int *ptr = &x;
+
+    cout << "Value of x: " << x << endl;
+    cout << "Address of x: " << &x << endl;
+    cout << "Pointer value (address): " << ptr << endl;
+    cout << "Value at pointer (*ptr): " << *ptr << endl;
+
+    return 0;
+}
