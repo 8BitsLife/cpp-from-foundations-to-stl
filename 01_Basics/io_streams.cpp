49a03dfaed9b5bd48f409d272221cd35cb53e39f
8BitsLife <gajanan.prsnl@gmail.com>
2026-03-03 21:39:44 +0530

    Update io_streams.cpp

--- 01_Basics/io_streams.cpp
+++ 01_Basics/io_streams.cpp
@@ -1,8 +1,25 @@
-
 #include <iostream>
 using namespace std;
 
-int main() {
-    cout << "Hello Gajanan" << endl;
+// Program: I/O Streams Demonstration
+// Purpose: Show how cin, cout, getline work
+
+int main()
+{
+    int age;
+    string name;
+
+    cout << "Enter your name: ";
+    getline(cin, name);   // takes full line including spaces
+
+    cout << "Enter your age: ";
+    cin >> age;
+
+    cout << "\n----- User Details -----\n";
+    cout << "Name: " << name << endl;
+    cout << "Age : " << age << endl;
+
+    cout << "\nEnd of program using iostream." << endl;
+
     return 0;
 }
