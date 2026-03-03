02abf8074e3a177cf2a13ca50c4abf048e02d1da
8BitsLife <gajanan.prsnl@gmail.com>
2026-03-03 20:37:13 +0530

    Refactor logical_operator.cpp by removing comments
    Removed comments and cleaned up the code structure.

--- 02_OOP/03_Operator_Overloading/logical_operator.cpp
+++ 02_OOP/03_Operator_Overloading/logical_operator.cpp
@@ -2,10 +2,7 @@
 #include <iostream>
 using namespace std;
 
-// ==========================================
 // Logical Operator Overloading Example
-// ==========================================
-
 class Number
 {
 private:
