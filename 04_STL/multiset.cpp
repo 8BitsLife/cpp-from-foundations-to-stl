47f6fe0d623250b96645eb9c236d1c4d5995c2d8
8BitsLife <gajanan.prsnl@gmail.com>
2026-03-03 18:13:44 +0530

    Add multiset example with element counting
    Demonstrate usage of multiset with duplicates and count.

--- 04_STL/multiset.cpp
+++ 04_STL/multiset.cpp
@@ -1,3 +1,20 @@
-
+#include <iostream>
 #include <set>
 using namespace std;
+
+int main()
+{
+    multiset<int> ms;
+
+    ms.insert(10);
+    ms.insert(10);
+    ms.insert(20);
+
+    cout << "Multiset elements (duplicates allowed): ";
+    for (int x : ms)
+        cout << x << " ";
+
+    cout << "\nCount of 10: " << ms.count(10);
+
+    return 0;
+}
