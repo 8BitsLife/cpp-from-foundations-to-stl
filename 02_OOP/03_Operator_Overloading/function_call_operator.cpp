2d4ca59e39f36ca976572232db640cbf18257a19
8BitsLife <gajanan.prsnl@gmail.com>
2026-03-03 20:35:40 +0530

    Remove comments in function_call_operator.cpp
    Removed comments from the function call operator example.

--- 02_OOP/03_Operator_Overloading/function_call_operator.cpp
+++ 02_OOP/03_Operator_Overloading/function_call_operator.cpp
@@ -2,10 +2,7 @@
 #include <iostream>
 using namespace std;
 
-// ==========================================
 // Function Call Operator Overloading Example
-// ==========================================
-
 class Multiplier
 {
 private:
