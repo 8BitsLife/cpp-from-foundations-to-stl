c5658734ee39e2b1fb37d29214bdf95b903284b0
8BitsLife <gajanan.prsnl@gmail.com>
2026-03-03 18:36:42 +0530

    Implement Student class with operator overloading

--- 02_OOP/03_Operator_Overloading/relational_equal.cpp
+++ 02_OOP/03_Operator_Overloading/relational_equal.cpp
@@ -1,7 +1,45 @@
+#include <iostream>
+using namespace std;
 
-class Num{
-    int val;
+// Student Class
+class Student
+{
+private:
+    int roll;
+    string name;
+
 public:
-    Num(int v):val(v){}
-    bool operator==(Num n){ return val==n.val; }
+    Student(int r, string n)
+    {
+        roll = r;
+        name = n;
+    }
+
+    void display() const
+    {
+        cout << "Roll: " << roll
+             << " Name: " << name << endl;
+    }
+
+    // Overloading == operator
+    bool operator==(const Student &obj) const
+    {
+        return (roll == obj.roll && name == obj.name);
+    }
 };
+
+int main()
+{
+    Student s1(101, "Aman");
+    Student s2(101, "Aman");
+
+    s1.display();
+    s2.display();
+
+    if (s1 == s2)
+        cout << "\nBoth students are same.\n";
+    else
+        cout << "\nStudents are different.\n";
+
+    return 0;
+}
