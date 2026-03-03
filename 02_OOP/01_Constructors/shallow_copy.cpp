601612bca7314a5d0b9394157253ab779998043f
8BitsLife <gajanan.prsnl@gmail.com>
2026-03-03 21:31:17 +0530

    Refactor Shallow class to Demo with shallow copy

--- 02_OOP/01_Constructors/shallow_copy.cpp
+++ 02_OOP/01_Constructors/shallow_copy.cpp
@@ -1,11 +1,40 @@
-
 #include <iostream>
 using namespace std;
 
-class Shallow {
-    int* data;
+// Shallow Copy Example
+
+class Demo
+{
+private:
+    int *ptr;
+
 public:
-    Shallow(int v){ data = new int(v); }
-    Shallow(const Shallow &s){ data = s.data; }
-    ~Shallow(){ delete data; }
+    Demo(int value)
+    {
+        ptr = new int(value);
+    }
+
+    // Default copy constructor (shallow copy)
+    // It copies the pointer, not the value
+
+    void display() const
+    {
+        cout << "Value: " << *ptr << endl;
+    }
+
+    ~Demo()
+    {
+        delete ptr;
+    }
 };
+
+int main()
+{
+    Demo d1(10);
+    Demo d2 = d1;   // shallow copy
+
+    d1.display();
+    d2.display();
+
+    return 0;
+}
