832449917b2bc9b7d3d85f856941d5ee987c3c0d
8BitsLife <gajanan.prsnl@gmail.com>
2026-03-03 18:05:33 +0530

    Update vector.cpp

--- 04_STL/vector.cpp
+++ 04_STL/vector.cpp
@@ -1,3 +1,29 @@
-
+#include <iostream>
 #include <vector>
 using namespace std;
+
+int main()
+{
+    // Creating a vector of integers
+    vector<int> numbers = {10, 20, 30};
+
+    // Adding elements
+    numbers.push_back(40);
+    numbers.push_back(50);
+
+    cout << "Vector elements: ";
+    for (int n : numbers)
+        cout << n << " ";
+
+    cout << "\nSize: " << numbers.size();
+    cout << "\nCapacity: " << numbers.capacity();
+
+    // Removing last element
+    numbers.pop_back();
+
+    cout << "\nAfter pop_back(): ";
+    for (int n : numbers)
+        cout << n << " ";
+
+    return 0;
+}
