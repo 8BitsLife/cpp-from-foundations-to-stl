e6bc7b9f7c4b9bcffd21747c8e54abce2b958f9f
8BitsLife <gajanan.prsnl@gmail.com>
2026-03-03 18:08:33 +0530

    Implement sorting, binary search, and reversing in vector

--- 04_STL/algorithms.cpp
+++ 04_STL/algorithms.cpp
@@ -1,3 +1,29 @@
-
+#include <iostream>
+#include <vector>
 #include <algorithm>
 using namespace std;
+
+int main()
+{
+    vector<int> v = {40, 10, 30, 20};
+
+    // Sorting
+    sort(v.begin(), v.end());
+
+    cout << "Sorted vector: ";
+    for (int x : v)
+        cout << x << " ";
+
+    // Binary search
+    if (binary_search(v.begin(), v.end(), 30))
+        cout << "\n30 found in vector";
+
+    // Reverse
+    reverse(v.begin(), v.end());
+
+    cout << "\nAfter reverse: ";
+    for (int x : v)
+        cout << x << " ";
+
+    return 0;
+}
