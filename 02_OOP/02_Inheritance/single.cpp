921ec1ce947ab4e4c94253f240b75fb5f6e906d4
8BitsLife <gajanan.prsnl@gmail.com>
2026-03-03 21:22:28 +0530

    Implement Person and Student classes with inheritance

--- 02_OOP/02_Inheritance/single.cpp
+++ 02_OOP/02_Inheritance/single.cpp
@@ -1,3 +1,52 @@
+#include <iostream>
+using namespace std;
 
-class A{};
-class B : public A{};
+// Base Class
+class Person
+{
+protected:
+    string name;
+    int age;
+
+public:
+    void setPerson(string n, int a)
+    {
+        name = n;
+        age = a;
+    }
+
+    void displayPerson() const
+    {
+        cout << "Name: " << name << endl;
+        cout << "Age : " << age << endl;
+    }
+};
+
+// Derived Class
+class Student : public Person
+{
+private:
+    int roll;
+
+public:
+    void setStudent(string n, int a, int r)
+    {
+        setPerson(n, a);  // calling base class function
+        roll = r;
+    }
+
+    void displayStudent() const
+    {
+        displayPerson();
+        cout << "Roll: " << roll << endl;
+    }
+};
+
+int main()
+{
+    Student s;
+    s.setStudent("Aman", 19, 101);
+    s.displayStudent();
+
+    return 0;
+}
